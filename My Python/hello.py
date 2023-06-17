052b32a7ef478a9462d1a048e80a91dfe33d1b28
Panha <chhelpanha2@gmail.com>
2023-06-17 09:36:44 +0700

    new

---
+++ My Python/hello.py
@@ -0,0 +1,9 @@
+
+print("Hello, Python")
+strName = input("Enter Your Name: ")
+birthYear = input("Enter Birth Year: ")
+
+print("Your Name: " + strName + "\t" + birthYear)
+age = 2023 - int(birthYear)
+
+print("Age: " + str(age))
